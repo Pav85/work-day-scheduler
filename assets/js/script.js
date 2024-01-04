5344eeaf1051a34cbe0c43673dcdf532251c9e6e
Pawel <pawel.werbowy@gmail.com>
2024-01-04 19:40:25 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,59 @@
-
-var currentDate = moment().format("dddd, MMMM Do");
-
-console.log(currentDate); // just a test
-
+// var currentDate = moment().format("dddd, MMMM Do");
 
 function currentTime() {
-
-return $("#currentDay").text(currentDate)
-
-};
+  var currentDate = moment().format("dddd, MMMM Do");
+  $("#currentDay").text(currentDate);
+}
 currentTime();
 
 // edit html
 for (var i = 9; i < 18; i++) {
+  var domEl = $(".container");
+  var timeBlock = $("<div>");
+  var row = $("<div>");
+  var hourBlock = $("<div>");
+  var textBlock = $("<textarea>");
+  var saveButton = $("<button>");
 
-var domEl = $(".container");
-var timeBlock =  $("<div>");
-var row = $("<div>");
-var hourBlock = $("<div>");
-var textBlock = $("<textarea>");
-var saveButton = $("<button>");
+  timeBlock.addClass("time-block");
+  row.addClass("row");
+  hourBlock.addClass("hour col-1");
+  hourBlock.text([i]);
+  hourBlock.css("background-color", "yellow");
+  hourBlock.css("padding-top", "30px");
 
+  textBlock.addClass("description col-10");
 
+  saveButton.addClass("saveBtn saveBtn i:hover col-1 fa fa-save");
 
-timeBlock.addClass("time-block");
-row.addClass("row");
-hourBlock.addClass("hour col-1");   
-hourBlock.text([i]);
-hourBlock.css("background-color", "yellow");
-hourBlock.css("padding-top", "30px");
-              
-textBlock.addClass("description col-10"); 
+  domEl.append(timeBlock);
+  timeBlock.append(row);
+  row.append(hourBlock, textBlock, saveButton);
 
-saveButton.addClass("saveBtn saveBtn i:hover col-1 fa fa-save"); 
+  let hour = moment().hours(); // gets current hour
 
-
-domEl.append(timeBlock);
-timeBlock.append(row);
-row.append(hourBlock, textBlock, saveButton);
-
-
-
-
-
-
-let hour = moment().hours();     // gets current hour 
-
-
-if (i < hour) {                          // if statement that color codes time blocks based on current time
+  if (i < hour) {
+    // if statement that color codes time blocks based on current time
     textBlock.addClass("past");
-} else if (i == hour) {
+  } else if (i == hour) {
     textBlock.addClass("present");
-} else {
+  } else {
     textBlock.addClass("future");
-};
+  }
 
-// local storage issue to be solved
+  // local storage issue to be solved
 
-saveButton.click(function () {
-   console.log("Hello")
-   var timeBlockEntries = JSON.parse(localStorage.getItem("timeBlockEntries")) || [];
+  saveButton.click(function () {
+    console.log("Hello");
+    var timeBlockEntries =
+      JSON.parse(localStorage.getItem("timeBlockEntries")) || [];
 
-   var hourBlockData = {
+    var hourBlockData = {
       hour: hourBlock.val([i]),
-      hourEntry: textBlock.val()
+      hourEntry: textBlock.val(),
+    };
 
-   }
-
-   timeBlockEntries.push(hourBlockData);
-   localStorage.setItem("timeBlockEntries", JSON.stringify(timeBlockEntries));
-   
-
-
-});
-
-
-
-
-};
-
-
-
-
+    timeBlockEntries.push(hourBlockData);
+    localStorage.setItem("timeBlockEntries", JSON.stringify(timeBlockEntries));
+  });
+}
